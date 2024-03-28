da2498d11d1454a1ee9e3516a9c333afedc52a91
Darius Baciu <baciu.darius01@gmail.com>
2024-03-28 11:43:49 +0200

    added particles canvas to home landing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,128 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useEffect } from "react";
 gsap.registerPlugin(useGSAP);
 
 
 export function Home() {
+    useEffect(() => {
+        
+const canvas = document.getElementById("canvas");
+canvas.width = 320;
+canvas.height = 320;
+const ctx = canvas.getContext("2d");
+
+class Vec2 extends Array {
+  
+  constructor (...values) {
+    switch(values.length) {
+      case 2:{
+        const v = values[0];
+        super(v, values[1]);
+        break;
+      }
+      case 1: {
+        const v = values[0];
+        super(v, v);
+        break;
+      }
+      default: {
+        super(2);
+        break;
+      }
+        
+  }
+  }
+  
+  get x(){ return this[0]; }
+  set x(value) { this[0] = value; }
+
+  get y() { return this[1]; }
+  set y(value) { this[1] = value; }
+  
+  add (b) {
+    this[0] += b[0];
+    this[1] += b[1];
+  }
+  
+  distance (b) {
+    return Vec2.distance(this, b);
+  }
+  
+  static distance(a, b) {
+    return Math.hypot(b[0] - a[0], b[1] - a[1]);
+  }
+}
+
+class Particle {
+  
+  constructor() {
+    this.speed = new Vec2(Math.random(), Math.random());
+    this.position = new Vec2(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
+    this.radius = Math.random() * 4 + 1;
+  }
+  
+  update() {
+    if (this.position.x > window.innerWidth || this.position.x < 0) {
+      this.speed.x *= -1;
+    }
+    if (this.position.y > window.innerHeight || this.position.y < 0) {
+      this.speed.y *= -1;
+    }
+    this.position.add(this.speed);
+  }
+}
+
+const particles = [];
+
+for (let i = 0; i < 100; i++) {
+  particles.push(new Particle());
+}
+
+const update = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const MAX_DISTANCE = (window.innerWidth + window.innerHeight) / 20;
+  
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    
+    for (let j = i; j < particles.length; j++) {
+      const distance = particles[i].position.distance(particles[j].position);
+      if (distance < MAX_DISTANCE) {
+          
+         ctx.strokeStyle = `rgba(3,233,210, ${1 - distance/MAX_DISTANCE})`;
+         ctx.beginPath();
+         ctx.moveTo(...particles[i].position);
+         ctx.lineTo(...particles[j].position);
+         ctx.stroke();
+      }
+    }
+    
+    ctx.fillStyle = "rgba(3,233,210,255)";
+    ctx.beginPath();
+
+    ctx.arc(
+      ...particles[i].position,
+      particles[i].radius,
+      0, 2 * Math.PI);
+    ctx.fill();
+  }
+  requestAnimationFrame(update);
+}
+
+update();
+    })
 
     useGSAP(() => {
-        //gsap.to(".box", { rotation: "+=360", duration: 3 });
-        const tl = gsap.timeline();
+        gsap.to(".box-rotation ", {   //animation heading description
+          rotation: "+=360", 
+          duration: 3 
+        });  
 
-        tl.from("h2", 1.8, {
+        const tl = gsap.timeline();  //title animation
+        tl.from("h2", 1.8, {  
         y: 100,
         ease: "power4.out",
         delay: 1,
@@ -24,14 +136,17 @@ export function Home() {
 });
 
     return (
-        <div name="home"className="w-full bg-gradient-to-b from-black from-90% via-black to-gray-800 via-10%">
-            <div className="h-[calc(140vh-25px)] max-w-screen-lg mx-auto flex flex-col items-center justify-center md:flex-row px-4 sm:h-[calc(100vh-80px)]">
+      <> 
+      <div id="hero" className="relative w-full  bg-gradient-to-b from-black from-90% via-black to-gray-800 via-10% -z-10">
+      <canvas id="canvas" className="absolute w-full h-screen" />
+      <div name="home" >
+              <div className="h-[calc(140vh-25px)] max-w-screen-lg mx-auto  z-50 flex flex-col items-center justify-center md:flex-row px-4 sm:h-[calc(100vh-80px)]">
                 <div className="flex flex-col justify-center h-full">
                     <h2 className="text-5xl sm:text-7xl font-bold text-white text-center sm:text-left">
                         I`m a Full Stack Developer
                     </h2>
-                    <p className="text-gray-500 py-4 max-w-md text-center sm:text-left">
-                    I`m an UI developer skilled in JavaScript Framework: Vue 3/Vue2, JQuery, HTML, CSS, Vuetify, Vuex, Pinia, Vite, PrimeVue | Advanced knowledge in PHP Framework: Laravel | Knowledge in WordPress
+                    <p className="text-gray-300 box-rotation py-4 max-w-md text-center sm:text-left">
+                    I`m an UI developer skilled in JavaScript Framework: Vue3/Vue2, JQuery, HTML, CSS, Vuetify, Vuex, Pinia, Vite, PrimeVue | Advanced knowledge in PHP Framework: Laravel | Knowledge in WordPress
                     </p>
         
                 <div className="flex items-center justify-center sm:justify-start">
@@ -49,10 +164,14 @@ export function Home() {
                 </div>
     
                 <div>
-                <img src={HeroImage} alt="My profile picture" className="rounded-2xl object-cover object-center w-full pt-6 sm:w-96"/>
+                <img src={HeroImage} alt="My profile picture" className="rounded-2xl relative z-50 object-cover object-center w-full pt-6 sm:w-96"/>
             </div>
-        </div>
-    </div>
+       </div>
+            </div> 
+      </div>
+      </>
+     
+     
     );
 }
 export default Home
