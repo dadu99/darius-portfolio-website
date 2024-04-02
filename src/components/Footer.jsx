eab146f1ee57dd61b3e0683180958c7bcc141e65
Darius Baciu <baciu.darius01@gmail.com>
2024-04-02 15:40:35 +0300

    added to the footer more social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,32 @@
-
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
 
 export function Footer() {
   return (
-  <footer className="bg-gradient-to-b from-gray-800 to-black">
-    <div className="w-full mx-auto max-w-screen-xl p-6">
-        <span className="flex justify-center gap-1 text-sm text-white sm:text-center dark:text-gray-400">
-          Created by <span className="italic">Darius Baciu</span>
-        </span>
-        
+  <footer className="bg-gradient-to-b from-gray-800 from-9% to-black via-91%">
+    <div className="w-full mx-auto max-w-screen-xl py-6">
+      <div className="flex flex-col gap-1 text-sm text-white sm:text-center">
+          <span className="flex justify-center gap-2 dark:text-gray-400">
+            Created by <span className="italic">Darius Baciu</span>
+          </span>
+
+          <div className="flex justify-center gap-2 text-sm text-white sm:text-center">
+
+            <a href="https://www.instagram.com/dariusbaciu_/" target="_blank" rel="noreferrer" className="hover:scale-110 cursor-pointer duration-200">
+              <FaInstagram size={20}/>
+            </a>
+
+            <a href="https://twitter.com/DariusDarius01" target="_blank" rel="noreferrer" className="hover:scale-110 cursor-pointer duration-200">
+              <FaXTwitter size={20}/>
+            </a>
+
+            <a href="https://www.facebook.com/baciu.darius01/" target="_blank" rel="noreferrer" className="hover:scale-110 cursor-pointer duration-200">
+              <FaFacebookF  size={20}/>
+            </a>
+
+        </div>
+      </div>
     </div>
   </footer>
   )
